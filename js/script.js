d11c7ad465a088a574694f923c5ab49428fc84fd
yuew355 <42750931+yuew355@users.noreply.github.com>
2018-08-28 20:27:27 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,36 @@
+var megaMenu = document.getElementById("overlay");
+var ham_layers = document.getElementsByClassName("ham-layer");
+var toggle = false;
+var topBun = document.getElementById("top_bun");
+var meat = document.getElementById("meat");
+var bottomBun = document.getElementById("bottom_bun");
+
+function toggleHamMenu(){
+	if (toggle == false){
+		//opens menu
+
+		topBun.style.top = "11px";
+		topBun.style.transform = "rotate(45deg)";
+		meat.style.opacity = "0";
+		bottomBun.style.top = "-11px";
+		bottomBun.style.transform = "rotate(-45deg)";
+		for(var i = 0; i < ham_layers.length; i++){
+			ham_layers[i].style.backgroundColor = "white";
+		}
+		toggle = true;
+		megaMenu.style.height = "100vh";
+
+	}else{
+		//close menu
+		megaMenu.style.height = "0vh";
+		topBun.style.top = "-0.05px";
+		topBun.style.transform = "rotate(0deg)";
+		bottomBun.style.top = "0.05px";
+		bottomBun.style.transform = "rotate(0deg)";
+		meat.style.opacity = "100";
+		for(var i = 0; i < ham_layers.length; i++){
+			ham_layers[i].style.backgroundColor = "#fbe14c";
+		}
+		toggle = false;
+	}
+}
